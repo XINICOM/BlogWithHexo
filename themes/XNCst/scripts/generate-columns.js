28336d679a763404679e37c6b4cb20e9fc403ebb
XINICOM <XINICOM@outlook.com>
2026-07-25 20:45:35 +0800

    DONE: column bottom link

--- themes/XNCst/scripts/generate-columns.js
+++ themes/XNCst/scripts/generate-columns.js
@@ -17,10 +17,35 @@ hexo.extend.filter.register("before_post_render", function (data) {
     // const titleSlug = slugize(originTitle);
     // const mappedSlug = columnTitleMapping[originTitle];
     // // 如果映射值为空字符串，则使用自动生成的 slug；否则使用映射值
-    const urlSlug = getColumnURLSlug(column.title);
+    const URL_Slug = getColumnURLSlug(column.title);
 
-    // 4. 将生成的 slug 挂载到文章对象上
-    data.column.path = "columns/" + urlSlug + "/";
+    //
+    data.column.slug = URL_Slug;
+    data.column.path = "columns/" + URL_Slug + "/";
+
+    //
+    const allPosts = hexo.locals.get("posts").toArray();
+    const sameColumnPosts = allPosts
+        .filter(
+            (p) => p.column && getColumnURLSlug(p.column.title) === URL_Slug,
+        )
+        .sort((a, b) => sortArticlesUnderColumn(a, b));
+    // data.column.allPosts = sameColumnPosts;
+    const currentIndex = sameColumnPosts.findIndex((p) => p.path === data.path);
+    console.log(URL_Slug, currentIndex);
+    data.column.currentPostIndex = currentIndex;
+    const prevPost =
+        currentIndex > 0 ? sameColumnPosts[currentIndex - 1] : null;
+    const nextPost =
+        currentIndex < sameColumnPosts.length - 1
+            ? sameColumnPosts[currentIndex + 1]
+            : null;
+    data.column.prevColumnPost = prevPost
+        ? { title: prevPost.title, path: prevPost.path }
+        : null;
+    data.column.nextColumnPost = nextPost
+        ? { title: nextPost.title, path: nextPost.path }
+        : null;
 
     return data;
 });
@@ -55,14 +80,17 @@ hexo.extend.generator.register("columns", function (locals) {
         const URL_slug = getColumnURLSlug(originTitle);
 
         // 获取该专栏下的所有文章，按 column.order 排序
-        var articles = columnGroups[originTitle].sort(function (a, b) {
-            // 如果有 column.order 字段，按它排序
-            if (a.column.order && b.column.order) {
-                return a.column.order - b.column.order;
-            }
-            // 否则按日期降序
-            return b.date - a.date;
-        });
+        // var articles = columnGroups[originTitle].sort(function (a, b) {
+        //     // 如果有 column.order 字段，按它排序
+        //     if (a.column.order && b.column.order) {
+        //         return a.column.order - b.column.order;
+        //     }
+        //     // 否则按日期降序
+        //     return b.date - a.date;
+        // });
+        var articles = columnGroups[originTitle].sort((a, b) =>
+            sortArticlesUnderColumn(a, b),
+        );
 
         // 创建页面数据
         results.push({
@@ -75,7 +103,7 @@ hexo.extend.generator.register("columns", function (locals) {
                     slug: URL_slug,
                 },
                 articles: articles,
-                total: articles.length,
+                // total: articles.length,
                 layout: ["column", "index"],
             },
             layout: ["column", "index"],
@@ -101,7 +129,19 @@ function getColumnURLSlug(originTitle) {
     const Title_slug = slugize(originTitle);
     const Title_map_slug = columnTitleMapping[originTitle];
     // console.log(originTitle, Title_map_slug);
-    return Title_map_slug && Title_map_slug !== ""
-        ? Title_map_slug
-        : Title_slug;
+    // return Title_map_slug &&
+    //     Title_map_slug !== "" &&
+    //     Title_map_slug !== undefined
+    //     ? Title_map_slug
+    //     : Title_slug;
+    return Title_map_slug || Title_slug;
+}
+
+function sortArticlesUnderColumn(a, b) {
+    // 如果有 column.order 字段，按它排序
+    if (a.column.order && b.column.order) {
+        return a.column.order - b.column.order;
+    }
+    // 否则按日期降序
+    return b.date - a.date;
 }
